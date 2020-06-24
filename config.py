90985e8c3ab35a20e0f3e07b2b2f8f81fcb0dff4
Matthew Donnelly <brilliantrocketscientist@outlook.com>
2020-06-24 09:50:39 -0700

    Part X: Email Support

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['brilliantrocketscientist@outlook.com']
+    ADMINS = ['matthew.donnelly@zybooks.com']
 
     POSTS_PER_PAGE = 25
